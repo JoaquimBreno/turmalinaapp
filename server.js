89dcd15493f694500334a4f4aafc0f27994a2da3
JoaquimBreno <winz993@gmail.com>
2021-12-14 23:05:00 -0300

    Deu certo

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const app = express();
-app.use(requireHTTPS);
-app.use(express.static('package.json'));
-app.get('/*', function(req, res) {
-    res.sendFile('./src/index.html', {root: 'package.json/'});
-});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/turmalina-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('./src/index.html', {root: 'dist/turmalina-app/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

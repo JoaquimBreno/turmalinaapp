779cadee2d99967f89fc7c023ae430e2cf9dc2c5
JoaquimBreno <winz993@gmail.com>
2021-12-15 15:53:25 -0300

    Commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('src/app/'));
+app.use(express.static('./dist/turmalina-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'src/app/'}),
+    res.sendFile('index.html', {root: 'dist/turmalina-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
